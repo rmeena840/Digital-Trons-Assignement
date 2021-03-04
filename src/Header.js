8a4f5fcd988be81ebd1fde7be7d9f5ef0a81bbf7
Ravinda <ravindra.iitism@gmail.com>
2021-03-04 19:02:04 +0530

    Header component added

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Container,
+  Jumbotron,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Header = () => {
+    return (
+        <>
+        <Jumbotron className="text-sm-left text-md-center">
+            <Container>
+            <h1 className="display-4">Let's Connect! <span role="img" aria-label="Close" >📅</span></h1>
+            <hr className="my-2" />
+            <p className="lead">
+                Tap or click on an available timeslot below to book an appointment.{" "}
+            </p>
+            <p>
+                Time slots marked in <span className="red-text">red</span> are
+                already booked.
+            </p>
+            </Container>
+        </Jumbotron>
+        </>
+    )
+};
+
+export default Header;
